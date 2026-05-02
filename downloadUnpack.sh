0a321b6509eeac9e835ef6ff31a3d768704e2fc6
Pedro H M França <pedro.henrique.moura.franca@cern.ch>
2026-05-02 16:27:30 +0200

    adding echo

--- downloadUnpack.sh
+++ downloadUnpack.sh
@@ -9,14 +9,14 @@ fi
 DATASET_DID="$1"
 DOWNLOAD_DIR="$DATASET_DID"
 
-
+echo "Downloading dataset: $DATASET_DID..."
 rucio download "$DATASET_DID"
 
-# if [ $? -ne 0 ]; then
-#     echo "Error downloading files."
-#     return 1 || exit 1
-# fi
-#
+if [ $? -ne 0 ]; then
+    echo "Error downloading files."
+    return 1 || exit 1
+fi
+
 if [ ! -d "$DOWNLOAD_DIR" ]; then
     echo "Error: Directory $DOWNLOAD_DIR does not exist."
     return 1 || exit 1
@@ -28,10 +28,13 @@ UNPACKED_DIR="extracted_results"
 ARCHIVE_DIR="original_backups"
 mkdir -p "$UNPACKED_DIR" "$ARCHIVE_DIR"
 
+echo "Starting to process .root.tgz files..."
+
 for file in *.root.tgz; do
     if [ -f "$file" ]; then
+        echo "-> Processing file: $file"
+        
         internal_files=$(tar -tf "$file")
-
         tar -xzf "$file" -C "$UNPACKED_DIR"
 
         if [ $? -eq 0 ]; then
@@ -41,6 +44,9 @@ for file in *.root.tgz; do
                 fi
             done
             mv "$file" "$ARCHIVE_DIR/"
+            echo "   [OK] $file extracted and moved to $ARCHIVE_DIR/"
+        else
+            echo "   [ERROR] Failed to extract $file"
         fi
     fi
 done
